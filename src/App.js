364be156bd47d28cd5bc0be0dd659db88fbeac27
Victor Nestor <victor.n.x35@gmail.com>
2021-10-04 16:57:46 -0400

    added edit and delete functions to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 /** This was made using the following tutorial: https://www.youtube.com/watch?v=dYjdzpZv5yc&t=2084s */
 
-import React, { useState, Fragment} from "react";
+import React, { useState, Fragment } from "react";
 import "./App.css";
-import {nanoid} from 'nanoid';
+import { nanoid } from "nanoid";
 /* calls api and getting data back, we are mocking data*/
 import data from "./mock-data.json";
 import ReadOnlyRow from "./components/ReadOnlyRow";
@@ -30,17 +30,14 @@ const App = () => {
     fullName: "",
     address: "",
     phoneNumber: "",
-    email: "", 
-  })
-
-
-
+    email: "",
+  });
 
   /* New state hook. */
   /* When editContactId is null, it means the user isnt editing any row. */
   /* We want to add a ternanry to render either the editable row, or the read only row
   depending if we have a editContactId or not. */
-  const [editContactId, setEditContactId] = useState(null); 
+  const [editContactId, setEditContactId] = useState(null);
 
   /* Function that gets called when a user changes a value in any of the inputs  */
   /* Arrow function that accepts the event  */
@@ -61,7 +58,7 @@ const App = () => {
   You can do this by adding onChange property in input. */
   /* When you specify a function call like this, React will 
   automatically pass in the event for us. */
-  
+
   /* Why we do this: It makes it easier to add new values to your form:
   You add a new input to your form, add a new property to the initial state,
   and call the event handler, and it'll all work.
@@ -70,7 +67,6 @@ const App = () => {
   for each input in our form, which can make the code repetitive and hard to follow.  */
 
   /* */
-
 
   const handleAddFormChange = (event) => {
     event.preventDefault();
@@ -95,7 +91,7 @@ const App = () => {
 
     /* Create a new object based on the new values so that we dont mutate the state. */
     /* Use spread operator to copy data from editFormData */
-    const newFormData = {...editFormData };
+    const newFormData = { ...editFormData };
     /* We use square brackets syntax on our object to update the value for a given field. */
     /* Pass in fieldName, and we set it equal to whatever value the user has typed, in this case, fieldValue */
     newFormData[fieldName] = fieldValue;
@@ -108,10 +104,7 @@ const App = () => {
     /* In our handleEditClick function, we already pass in the event and the contact as parameters. */
     /* All we have to do it take the values from the contact object and save it to our edit form data. */
     /* Jump to line 155 */
-
-  
-  } 
-
+  };
 
   /* */
   /* We will add a function that gets called when the form is submitted */
@@ -136,70 +129,67 @@ const App = () => {
       address: addFormData.address,
       phoneNumber: addFormData.phoneNumber,
       email: addFormData.email,
-    }
+    };
 
-    const newContacts = [...contacts, newContact]
-    setContacts(newContacts)
-    
-  }
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+  };
 
   /**
-   * Similar to the add new contact functionality, we will create a new event handler function. 
-   * 
+   * Similar to the add new contact functionality, we will create a new event handler function.
+   *
    * We create an arrow function that accepts an event.
    * preventDefault so the page doesnt try to do a POST on form submit.
-   * 
+   *
    * Just like before, we create a new object based on the new data stored in the edit form.
-   * 
+   *
    * We also want to keep the id in place as we need it to determine what row we are editing.
-   * 
+   *
    * We create a newContacts array so we dont mutate the state.
    * We copy the existing contacts by using the spread operator, "...".
    * Instead of adding contacts to the end of the array, we want to replace the contact object
-   * in the contacts array with this new object we just created. 
-   * 
+   * in the contacts array with this new object we just created.
+   *
    * First we need to get the index of the row which we are editing:
    * We use findIndex() and we'll pass in a function.
    * findIndex() will return an index based on a condition that we pass in.
    * To find index we pass the contact to our arrow function and then we want to say,
    * find the index of the contact.id is equal to the editContactId.
    * The editContactId is the row we are editing, we want to get the index of that row
-   * in the contacts array. 
-   * 
+   * in the contacts array.
+   *
    * Now that we have the index, we can update the array at the given position.
    * At the index in our newContacts array, so the contact at this postion in the newContacts array
    * is going to be equal to our new contact that we created, editedContact.
-   * 
+   *
    * Lastly, we set out new array into state and set our edit contact id to null as we are finished
    * editing and this will hide the editable row.
-   * 
-   * 
+   *
+   * Dont forget to add function to the form. Add it to the edit contact form which wraps the table.
+   * onSubmit={handleEditFormSubmit}
+   *
    */
-
 
   const handleEditFormSubmit = (event) => {
     event.preventDefault();
 
     const editedContact = {
       id: editContactId,
-      fullname: editFormData.fullName,
+      fullName: editFormData.fullName,
       address: editFormData.address,
       phoneNumber: editFormData.phoneNumber,
-      email: editFormData.email
-    }
+      email: editFormData.email,
+    };
 
     const newContacts = [...contacts];
 
-    const index = contacts.findIndex((contact)=> contact.id === editContactId);
+    const index = contacts.findIndex((contact) => contact.id === editContactId);
 
     newContacts[index] = editedContact;
 
     setContacts(newContacts);
     setEditContactId(null);
-
-  }
-
-
+  };
 
   /* */
   /* This will be called when the user clicks on the edit button. */
@@ -213,101 +203,141 @@ const App = () => {
 
     /* Create a new object, formValues, this will have the same properties as our editFormData state object,
     same as the ones we've been working with so far. */
-    
+
     const formValues = {
       fullName: contact.fullName,
       address: contact.address,
       phoneNumber: contact.phoneNumber,
-      email: contact.email
-    }
+      email: contact.email,
+    };
 
     /* Set to state. */
     /** So we have a way to save the values to state and an event handler to update the state */
     /** We can pass these to our editable row component. */
     /** Jump to EditableRow component. */
     setEditFormData(formValues);
+  };
 
-  }
+  /**
+   * To cancel, all we want to do is set editContactId back to null.
+   * The editable row only renders when editContactId matches given any contact id in our contacts array.
+   * Otherwise, the read only row is rendered.
+   *
+   * Pass this event handler to the editable row as a prop.
+   * In EditableTow.js, destructure it, and add a new button.
+   */
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  };
 
+  /**
+   * To delete, we ill remove the row from state, and react will re render the component with the new array, with the given row removed.
+   *
+   * Create an arrow function that accepts a contactId. This will be used to find the index of that contact, and remove them from contacts array.
+   * newContacts is equal to a copy of the contacts array, copied using spread operator, to not mutate the state.
+   * We then find the index using findIndex(), use arrow function to get the current contact as a parameter.
+   *
+   * Next, we will use the splice method to remove the contact object at the given index in the array.
+   * The splice functoin takes two parameters, the index of the item in the array, and how many elements you want to remove.
+   * Finally, save into state.
+   *
+   * Delete button will live in read only row component.
+   * Destructure in ReadOnlyRow.js
+   * Add a button
+   */
+  const handleDeleteClick = (contactId) => {
+    const newContacts = [...contacts];
 
+    const index = contacts.findIndex((contact) => contact.id === contactId);
 
-  /* className helps position the container */
+    newContacts.splice(index, 1);
+
+    setContacts(newContacts);
+  };
 
   return (
     <div className="app-container">
-      {/* table tag, indicates start of the table */}
-      <table>
-        {/* Table head tag: contains column heading */}
-        <thead>
-          {/* Table Row tag */}
-          <tr>
-            {/* table heading tag */}
-            <th>Name</th>
-            <th>Address</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        {/* Table body tag */}
-        <tbody>
-          {/* Map data using an arrow function */}
-          {/* Map function outputs a row for each contact */}
-          {/* As the map function flips through the contacts array, it'll give us 
+      {/* className helps position the container */}
+      <form onSubmit={handleEditFormSubmit}>
+        {/* table tag, indicates start of the table */}
+        <table>
+          {/* Table head tag: contains column heading */}
+          <thead>
+            {/* Table Row tag */}
+            <tr>
+              {/* table heading tag */}
+              <th>Name</th>
+              <th>Address</th>
+              <th>Phone Number</th>
+              <th>Email</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          {/* Table body tag */}
+          <tbody>
+            {/* Map data using an arrow function */}
+            {/* Map function outputs a row for each contact */}
+            {/* As the map function flips through the contacts array, it'll give us 
         access to the current contact object it is currently on and passes it
       to our function as a varaible. We name this variable, contact */}
-          {/* We now have access to the contact object 
+            {/* We now have access to the contact object 
             we can display the data in our table row */}
-          {/* All these properties are coming from the contact object which
+            {/* All these properties are coming from the contact object which
               is part of the mock data array */}
 
-          {/* After creating ReadOnlyRow component */}
-          {/* We are rendering our new component in place of our old code.
+            {/* After creating ReadOnlyRow component */}
+            {/* We are rendering our new component in place of our old code.
           and we are passing in contact as a prop. */}
-          {/* Don't forget to import our component. */}
+            {/* Don't forget to import our component. */}
 
-          {/* After creating EditableRow component */}
-          {/* Cannot have two children components stacked, must use React Fragment. */}
-          {/* Ideally, we want to wrap editable row in a form tag so that we can handle the 
+            {/* After creating EditableRow component */}
+            {/* Cannot have two children components stacked, must use React Fragment. */}
+            {/* Ideally, we want to wrap editable row in a form tag so that we can handle the 
           form submit and capture the inputs. Unfortunately, we cannot. */}
-          {/* Instead, we will wrap the entire table in the form tags. This is ok as we will
+            {/* Instead, we will wrap the entire table in the form tags. This is ok as we will
           only have one set of form inputs displayed being displayed in the DOM at any one time. */}
-          {/* Form inputs will not be duplicated with this approach.  */}
+            {/* Form inputs will not be duplicated with this approach.  */}
 
-          {/* After creating editContactId state hook */}
-          {/* As the map function flips through the array, 
+            {/* After creating editContactId state hook */}
+            {/* As the map function flips through the array, 
           if the id of the current contact object matches the id stored in state 
           in the editConactId state hook, then it will render the editable row.
           If not, it will render the read only row. */}
-          {/* We want to add an edit button to each row, which will update our editContactId state hook. */}
+            {/* We want to add an edit button to each row, which will update our editContactId state hook. */}
 
-          {/* After creating handleEditClick event listener */}
-          {/* We pass this fucntion to our ReadOnlyRow component as a prop
+            {/* After creating handleEditClick event listener */}
+            {/* We pass this fucntion to our ReadOnlyRow component as a prop
           as this is where our edit button is going to be. */}
-          {/* The editable row is essentially a form, similar to our addNewContact form. */}
+            {/* The editable row is essentially a form, similar to our addNewContact form. */}
 
-          {/* To create a save and cancel feature, and repopulate the form: */}
-          {/* We will start by creating a new state object to hold the form data 
+            {/* To create a save and cancel feature, and repopulate the form: */}
+            {/* We will start by creating a new state object to hold the form data 
           for when we are editing a given row. */}
 
-          {/** In the editable row component, we pass our form data, editFormData */}
-          {/** We will pass in our function to update the form values in state when the 
-           * the user types by saying handleEditFormChange
-           */}
-          {contacts.map((contact) => (
-            <Fragment>
-              
-              {editContactId === contact.id ? (
-              <EditableRow editFormData={editFormData} handleEditFormChange={handleEditFormChange}/>
-               ) : (
-               <ReadOnlyRow contact={contact} handleEditClick={handleEditClick}/> 
-               )}
-              
-            </Fragment>
-            
-          ))}
-        </tbody>
-      </table>
+            {/** In the editable row component, we pass our form data, editFormData */}
+            {/** We will pass in our function to update the form values in state when the
+             * the user types by saying handleEditFormChange
+             */}
+            {contacts.map((contact) => (
+              <Fragment>
+                {editContactId === contact.id ? (
+                  <EditableRow
+                    editFormData={editFormData}
+                    handleEditFormChange={handleEditFormChange}
+                    handleCancelClick={handleCancelClick}
+                  />
+                ) : (
+                  <ReadOnlyRow
+                    contact={contact}
+                    handleEditClick={handleEditClick}
+                    handleDeleteClick={handleDeleteClick}
+                  />
+                )}
+              </Fragment>
+            ))}
+          </tbody>
+        </table>
+      </form>
       {/* We will add contact by using a simple html form */}
       {/* We need to store form values in state, and update values as
       the user types */}
--- src/components/ReadOnlyRow.js
+++ src/components/ReadOnlyRow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 /* This was cut and pasted from App.js */
 /* We will be pass these in as props. */
@@ -9,19 +9,36 @@ will can render the details based on the contact objects. */
 /* After adding edit button */
 /* Whenever the button is clicked, the state changes and sets the editContactId,
 the component re-renders because the state changes and our ternary operator runs.  */
-const ReadOnlyRow = ({contact, handleEditClick}) => {
-    return(
-        
-        <tr>
-              <td>{contact.fullName}</td>
-              <td>{contact.address}</td>
-              <td>{contact.phoneNumber}</td>
-              <td>{contact.email}</td>
-              <td>
-                  <button type="button" onClick={(event)=>handleEditClick(event,contact)}>Edit</button>
-              </td>
-            </tr>
-    )
-}
 
-export default ReadOnlyRow
+/**
+ * After adding delete button
+ * Destruct handleDeleteClick
+ * Pass in arrow function which calls handleDeleteClick, pass in contact.id, which you get from contact that was passed in.
+ * Whenever you call a function similar to handleDeleteClick and pass porameters, you must do it using an arrow function,
+ * as this prevents the function that we're calling to be invoked straight away.
+ * We want the function to be called when the onClick happens.
+ *
+ */
+const ReadOnlyRow = ({ contact, handleEditClick, handleDeleteClick }) => {
+  return (
+    <tr>
+      <td>{contact.fullName}</td>
+      <td>{contact.address}</td>
+      <td>{contact.phoneNumber}</td>
+      <td>{contact.email}</td>
+      <td>
+        <button
+          type="button"
+          onClick={(event) => handleEditClick(event, contact)}
+        >
+          Edit
+        </button>
+        <button type="button" onClick={() => handleDeleteClick(contact.id)}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default ReadOnlyRow;
